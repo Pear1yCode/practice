37e0a180bb730dab75e50ff69ec6a7459121c84f
Pear1y <gh_wns@naver.com>
2024-09-14 21:32:59 +0900

    Add files via upload

---
+++ Java/conditional/maketree/Application.java
@@ -0,0 +1,8 @@
+package Java.testQuestion.maketree;
+
+public class Application {
+    public static void main(String[] args) {
+    Tree tree = new Tree();
+        tree.tr();
+    }
+}
---
+++ Java/conditional/maketree/Tree.java
@@ -0,0 +1,15 @@
+package Java.testQuestion.maketree;
+
+import java.util.Scanner;
+
+public class Tree {
+
+    public void tr () {
+        Scanner sc = new Scanner(System.in);
+        int tree = sc.nextInt();
+        String trees = "*";
+        for (int i = 0; i <= tree; i ++) {
+            System.out.println(trees.repeat(i));
+                }
+            }
+        }
