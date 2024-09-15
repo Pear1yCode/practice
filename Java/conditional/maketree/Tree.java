a189fad6285fdd1f489ec827adbc70ec998e2bf1
Pear1y <gh_wns@naver.com>
2024-09-15 20:36:19 +0900

    Update Tree.java

--- Java/conditional/maketree/Tree.java
+++ Java/conditional/maketree/Tree.java
@@ -3,12 +3,15 @@ package Java.testQuestion.maketree;
 import java.util.Scanner;
 
 public class Tree {
+    StringBuilder sb = new StringBuilder();
 
     public void tr () {
         Scanner sc = new Scanner(System.in);
+        System.out.print("트리 만들기 : ");
         int tree = sc.nextInt();
         String trees = "*";
         for (int i = 0; i <= tree; i ++) {
+            System.out.print(sb.insert(0, " *"));
             System.out.println(trees.repeat(i));
                 }
             }
