d834e6777e269fc5d8d34032bba0ea3933e602d3
Pear1y <gh_wns@naver.com>
2025-02-20 15:39:05 +0900

    Add files via upload

--- MemoryOverclock/php/header.php
+++ MemoryOverclock/php/header.php
@@ -7,20 +7,18 @@
     <link rel="stylesheet" href="../css/header.css">
 </head>
 <body>
-<header>
-    <div class="container">
-        <div class="logo">
-            <a href="/">홈페이지</a>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="/">홈</a></li>
-                <li><a href="/about.php">소개</a></li>
-                <li><a href="/services.php">서비스</a></li>
-                <li><a href="/contact.php">연락처</a></li>
-            </ul>
-        </nav>
+<header class="sidebar">
+    <div class="logo">
+        <a href="/">홈페이지</a>
     </div>
+    <nav>
+        <ul>
+            <li><a href="/">홈</a></li>
+            <li><a href="/about.php">소개</a></li>
+            <li><a href="/services.php">서비스</a></li>
+            <li><a href="/contact.php">연락처</a></li>
+        </ul>
+    </nav>
 </header>
 </body>
 </html>
